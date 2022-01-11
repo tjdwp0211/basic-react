59f01b5bf843a23bea9fb073c85c42fdd7294e4b
김성제 <rlatjdwp0211@naver.com>
2022-01-11 15:18:40 +0900

    REACT 26강 로딩 Spinner 추가하기

---
+++ src/movie-component/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Spinner = () => {
+  return (
+    <div className='d-flex justify-content-center'>
+        <div 
+            className="spinner-border" 
+            role="status"
+            style={{width: '3rem', height: '3rem'}}>
+            <span className="visually-hidden">
+                Loading...
+            </span>
+        </div>
+    </div>
+  );
+};
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,21 +1,26 @@
 import { React, useState ,useEffect } from "react";
 import axios from "axios";
 import UserList from "../movie-component/UserList";
+import { Spinner } from "../movie-component/Spinner";
 
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(res => {
                 setUsers(res.data);
+                setLoading(false);
             });
     }, []);
-    
+
     return (
 
         <>
             <h1>Users1</h1>
-            <UserList users={users}/>
+            {loading ? <Spinner /> : null}
+            <UserList users={users}/> 
         </>
     );
 };
